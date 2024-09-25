d69a1441433d69461d57e7d21f8bdcf4949da015
ifme18 <mainakenken188@gmail.com>
2024-09-25 14:23:22 +0300

    hhh

---
+++ index.js
@@ -0,0 +1,11 @@
+// 1. divide function expression
+const divide = function() {
+    return 2000 / 100;
+  };
+  
+  // 2. square arrow function
+  const square = (x) => x * x;
+  
+  // 3. add arrow function
+  const add = (a, b) => a + b;
+  
